a131dfe5e9f4fbf7ad0c754580ce21baf4f7dfae
JawaharMurali <JawaharMurali@DESKTOP-UKO5G7H>
2020-07-15 11:27:18 +0530

    Latest

--- src/test/java/Test/NewTest.java
+++ src/test/java/Test/NewTest.java
@@ -13,26 +13,26 @@ import org.testng.annotations.Test;
 
 
 public class  NewTest{
-	
-	 public WebDriver driver;
-     @BeforeClass
-     public void luanchApplication() {
-         driver = new ChromeDriver();
-         driver.manage().window().maximize();
-         driver.get("http://demowebshop.tricentis.com/login");
-     }
-     @Test(priority = 1)
-     public void Login() {
-         driver.findElement(By.id("Email")).sendKeys("manipal822@gmail.com");
-         driver.findElement(By.id("Password")).sendKeys("manipal123");
-         driver.findElement(By.xpath("//input[@type='submit'and @value='Log in']")).click();
-     }
-     @Test(priority = 2)
-     public void Logout() {
-         driver.findElement(By.linkText("Log out")).click();
-     }
-     @AfterClass
-     public void closeApplication() {
-         driver.close();
-     }
+	public WebDriver driver;
+    @BeforeClass
+    public void luanchApplication() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\JawaharMurali\\Desktop\\NJ\\LPB23\\Selenium Jar files\\Chrome webDriver\\chromedriver_win32 (2)\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://demowebshop.tricentis.com/login");
+    }
+    @Test(priority = 1)
+    public void Login() {
+        driver.findElement(By.id("Email")).sendKeys("manipal822@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("manipal123");
+        driver.findElement(By.xpath("//input[@type='submit'and @value='Log in']")).click();
+    }
+    @Test(priority = 2)
+    public void Logout() {
+        driver.findElement(By.linkText("Log out")).click();
+    }
+    @AfterClass
+    public void closeApplication() {
+        driver.close();
+    }
 }
